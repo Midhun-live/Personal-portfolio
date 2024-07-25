37e46e77ea966bc0e1a4b8acc722e51d46b22d6c
Midhun Chakkaravarthy <uiintern.smartail@gmail.com>
2024-07-25 23:28:46 +0530

    navbar-component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import LeftSide from "@/components/LeftSide";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>Portfolio</main>;
+  return (
+    <>
+      <Head>
+        <title>Midhun Chakkaravarthy</title>
+      </Head>
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+        <Navbar />
+        <div
+          className="w-full h-screen font-bodyFont bg-bodyColor text-textLight
+        overflow-x-hidden overflow-x-scroll"
+        >
+          <div>
+            <LeftSide />
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
 }

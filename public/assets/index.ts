c29320fad31c3d41476155f33c22e2fe214a7fa0
Midhun <midhunchakkaravarthybaz@gmail.com>
2024-11-30 17:25:52 +0530

    added a preloader

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,5 +1,6 @@
 import logo from './images/favicon.svg';
 import profile from './images/midhun.jpeg';
 import project_1 from './images/project-1.png'
+import loader from './images/hexagonal.gif'
 
-export {logo,profile,project_1};
+export {logo,profile,project_1,loader};

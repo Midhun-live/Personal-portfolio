b65e01a4fe36b403a80a98d882581f638b4641cb
Midhun-Chakkaravarthy <midhunchakkaravarthybaz@gmail.com>
2025-06-12 17:46:27 +0530

    again got updated with resume and contents

--- components/works/Sayvai.tsx
+++ components/works/Sayvai.tsx
@@ -10,7 +10,7 @@ const Sayvai = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Software Developer{" "}
+        Full Stack Developer{" "}
         <span className="text-textGreen tracking-wide">@Sayvai Softwares</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
--- components/works/YokoStyles.tsx
+++ components/works/YokoStyles.tsx
@@ -10,7 +10,7 @@ const YokoStyles = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Full Stack Developer Consultant{" "}
+        Full Stack Developer{" "}
         <span className="text-textGreen tracking-wide">@YokoStyles</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
